a7098552c815a1756191333a7c2180aeb822d49d
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2025-01-11 23:10:10 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
+import App from './App';
+import './index.scss';
+
+const tagManagerArgs = {
+	gtmId: 'GTM-K4QB4JRZ'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
